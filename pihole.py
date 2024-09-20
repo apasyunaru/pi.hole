b2ff289dad142b9e811092e7e0bd5bb19b0d7f98
Emre Demirdağ <emredemrdag@gmail.com>
2024-09-20 20:18:02 +0300

    pi hole scan

---
+++ pihole.py
@@ -0,0 +1,24 @@
+from shodan import Shodan
+import requests
+api = Shodan('API_KEY')
+
+def url_ok(url):
+    r = requests.head("http://" + url)
+    return r.status_code == 200
+
+def check_page(url):
+    r = requests.get("http://" + url + "/admin/")
+    return "Pi-hole" in r.text
+
+def pruneIPS(vulnerableIPs):
+    for i in vulnerableIPs:
+        if not url_ok(i):
+            if not check_page(i):
+                vulnerableIPs.remove(i)
+    return vulnerableIPs
+
+result = api.search("pi-hole")
+
+VulnerableIP = []
+for service in result['matches']:
+    VulnerableIP.append(service['ip_str'])
